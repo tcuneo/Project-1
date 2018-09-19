50c8010f55595bb66c1427e15150974b6977e8bd
IanV27 <ianvillarroel@gmail.com>
2018-09-18 20:37:05 -0400

    I have updated the Tile javascript code, updated directional commands, up, down,left and right

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,9 +32,83 @@ var gameData = [
     direction: 'right'
   };
 
+  function createTiles(data) {
+
+    // We'll keep the DOM elements in an array.
+    var tilesArray = [];
+  
+    // var's take one row at a time...
+    for (var row of data) {
+  
+    // Then look at each column in that row.
+    for (var col of row) {
+
+      // We create a game tile as a div element.
+      var tile = document.createElement('div');
+
+      // We assign every tile the class name tile.
+      tile.classList.add('tile');
+
+      // Now, depending on the numerical value,
+      // we need to add a more specific class.
+      if (col === WALL) {
+        tile.classList.add('wall');
+
+      } else if (col === COIN) {
+        tile.classList.add('coin');
+
+      } else if (col === GROUND) {
+        tile.classList.add('ground');
+
+      } else if (col === PACMAN) {
+        tile.classList.add('pacman');
+
+        // For Pacman, we will add yet another
+        // class for the direction pacman is facing.
+        tile.classList.add(pacman.direction);
+
+      }
+
+      // Now that our individual tile is configured,
+      // we add it to the tilesArray.
+      tilesArray.push(tile);
+    }
+
+    // Once we reach the end of a row, we create a br element,
+    // which tells the browser to create a line break on the page.
+    var brTile = document.createElement('br');
+
+    // We then add that br element to the tilesArray.
+    tilesArray.push(brTile);
+  }
+  
+    // At the end of our function, we return the array
+    // of configured tiles.
+    return tilesArray;
+  }
+  
+  // This function creates a map element, fills it with tiles,
+  // and adds it to the page.
+  function drawMap() {
+    map = document.createElement('div');
+  
+    var tiles = createTiles(gameData);
+    for (var tile of tiles) {
+      map.appendChild(tile);
+    }
+  
+    document.body.appendChild(map);
+  }
+  
+  // This function removes the map element from the page.
+  function eraseMap() {
+    document.body.removeChild(map);
+  }
+
+
 // Movement Functions
   function moveDown() {
-    pacman.direction = '';
+    pacman.direction = "down";
     if (gameData[pacman.y+1][pacman.x] !== WALL) {
       gameData[pacman.y][pacman.x] = GROUND;
       pacman.y = pacman.y + 1 ;
@@ -43,7 +117,7 @@ var gameData = [
   }
   
   function moveUp() {
-    pacman.direction = '';
+    pacman.direction = "up";
     if (gameData[pacman.y-1][pacman.x] !== WALL) {
       gameData[pacman.y][pacman.x] = GROUND;
       pacman.y = pacman.y - 1;
@@ -52,7 +126,7 @@ var gameData = [
   }
   
   function moveLeft() {
-    pacman.direction = '';
+    pacman.direction = "left";
     if (gameData[pacman.y][pacman.x-1] !== WALL) {
       gameData[pacman.y][pacman.x] = GROUND;
       pacman.x = pacman.x - 1 ;
@@ -61,7 +135,7 @@ var gameData = [
   }
   
   function moveRight() {
-    pacman.direction = '';
+    pacman.direction = "right";
     if (gameData[pacman.y][pacman.x+1] !== WALL) {
       gameData[pacman.y][pacman.x] = GROUND;
       pacman.x = pacman.x + 1 ;
