620aa7ebfffdc7e07e63f2f6e54095e58e4cff2e
Kirsten Millan <poppy0m9@gmail.com>
2018-09-25 17:21:46 -0400

    add a dog

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,6 +62,97 @@ $(document).ready(function() {
   });
   /*end of js for user adding new image for PacDog*/
 
+  /*API call to add alternate dog choice*/
+  $(document).ready(function() {
+
+    var animals = [
+      "dog", "Pacman"
+    ];
+  
+    // button creation
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+      $(areaToAddTo).empty();
+  
+      for (var i = 0; i < arrayToUse.length; i++) {
+        var a = $("<button>");
+        a.addClass(classToAdd);
+        a.attr("data-type", arrayToUse[i]);
+        a.text(arrayToUse[i]);
+        $(areaToAddTo).append(a);
+      }
+  
+    }
+  
+    $(document).on("click", ".animal-button", function() {
+      $("#animals").empty();
+      $(".animal-button").removeClass("active");
+      $(this).addClass("active");
+  
+      var type = $(this).attr("data-type");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=BkXVXu1cY1zcq3307RTRx8l82CE0ThYq";
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          var results = response.data;
+  
+          for (var i = 0; i < results.length; i++) {
+            var animalDiv = $("<div class=\"animal-item\">");
+  
+            var rating = results[i].rating;
+  
+            var p = $("<p>").text("Rating: " + rating);
+  
+            var animated = results[i].images.fixed_height.url;
+            var still = results[i].images.fixed_height_still.url;
+  
+            var animalImage = $("<img>");
+            animalImage.attr("src", still);
+            animalImage.attr("data-still", still);
+            animalImage.attr("data-animate", animated);
+            animalImage.attr("data-state", "still");
+            animalImage.addClass("animal-image");
+  
+            animalDiv.append(p);
+            animalDiv.append(animalImage);
+  
+            $("#animals").append(animalDiv);
+          }
+        });
+    });
+  
+    $(document).on("click", ".animal-image", function() {
+  
+      var state = $(this).attr("data-state");
+  
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      }
+      else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+  
+    $("#add-animal").on("click", function(event) {
+      event.preventDefault();
+      var newAnimal = $("input").eq(0).val();
+  
+      if (newAnimal.length > 2) {
+        animals.push(newAnimal);
+      }
+  
+      populateButtons(animals, "animal-button", "#animal-buttons");
+  
+    });
+  
+    populateButtons(animals, "animal-button", "#animal-buttons");
+  });
+/*End of Giphy API*/  
+
 
   // Function to create tiles
   function createTiles(data) {
@@ -301,7 +392,7 @@ url:  queryURL,
 method: "GET"
 }).then(function(response) {
       console.log(response);
-    });
+ 
 
    var player;
    function onYouTubeIframeAPIReady() {
@@ -316,7 +407,7 @@ method: "GET"
                loop: 1,
                controls: 0,
                showinfo: 0,
-               autohide: 1,
+               autohide: 0,
                enablejsapi: 1,
                modestbranding: 0,
                playlist: 'PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq',
@@ -329,7 +420,7 @@ method: "GET"
            }
        });
    }
-   
+  });
    function onPlayerStateChange(el) {
        if(el.data === 1) {
            jQuery('#ytplayer').hide();
