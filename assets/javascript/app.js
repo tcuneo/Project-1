1d9b33500cb8cc4dba9f5f0d12ceb8d84cbed686
Taylor <tcuneo5@gmail.com>
2018-09-25 20:08:00 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   
   var backgroundDog = "";
-
   /*creating the game board - pretty simple at this point*/
-
   var gameData = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1],
@@ -19,14 +17,11 @@ $(document).ready(function() {
     [1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
-
   const WALL = 1;
   const COIN = 2;
   const GROUND = 3;
   const PACMAN = 5;
-
   var map;
-
   //starting location of pacman, "0" being first count//
   var pacman = {
     x: 10,
@@ -34,8 +29,6 @@ $(document).ready(function() {
     score: 0,
     direction: "right"
   };
-
-
   /*js for user adding new image for PacDog*/
   document.getElementById("upload-file").addEventListener("change", function() {
     var file;
@@ -52,16 +45,13 @@ $(document).ready(function() {
           backgroundDog = img;
           destination.append(img);
         };
-
         reader.readAsDataURL(file);
       }
     }
   });
   // End of js for user adding new image for PacDog
-
   /*API call to add alternate dog choice*/
   $(document).ready(function() {
-
     var animals = [
       "dog", "Pacman"
     ];
@@ -149,23 +139,18 @@ $(document).ready(function() {
     populateButtons(animals, "animal-button", "#animal-buttons");
   });
 /*End of Giphy API*/  
-
-
   // Function to create tiles
   function createTiles(data) {
     // We'll keep the DOM elements in an array.
     var tilesArray = [];
-
     // var's take one row at a time...
     for (var row of data) {
       // Then look at each column in that row.
       for (var col of row) {
         // We create a game tile as a div element.
         var tile = document.createElement("div");
-
         // We assign every tile the class name tile.
         tile.classList.add("tile");
-
         // Now, depending on the numerical value,
         // we need to add a more specific class.
         if (col === WALL) {
@@ -176,51 +161,40 @@ $(document).ready(function() {
           tile.classList.add("ground");
         } else if (col === PACMAN) {
           tile.classList.add("pacman");
-
           // For Pacman, we will add yet another
           // class for the direction pacman is facing.
           tile.classList.add(pacman.direction);
         }
-
         // Now that our individual tile is configured,
         // we add it to the tilesArray.
         tilesArray.push(tile);
       }
-
       // Once we reach the end of a row, we create a br element,
       // which tells the browser to create a line break on the page.
       var brTile = document.createElement("br");
-
       // We then add that br element to the tilesArray.
       tilesArray.push(brTile);
     }
-
     // At the end of our function, we return the array
     // of configured tiles.
     return tilesArray;
       
   }
-
 $("")
-
   // This function creates a map element, fills it with tiles,
   // and adds it to the page.
   function drawMap() {
     map = document.createElement("div");
-
     var tiles = createTiles(gameData);
     for (var tile of tiles) {
       map.appendChild(tile);
     }
-
     document.body.appendChild(map);
   }
-
   // This function removes the map element from the page.
   function eraseMap() {
     document.body.removeChild(map);
   }
-
   // Movement Functions
   function movePacman(direction) {
     pacman.direction = direction;
@@ -244,9 +218,7 @@ $("")
         pacman.score += 5;
         console.log(pacman.score);
         $("#highscore-div").html("Highscore: " + pacman.score);
-
       }
-
       // sets the previous position to GROUND
       gameData[pacman.y][pacman.x] = GROUND;
       pacman.x = x;
@@ -254,7 +226,6 @@ $("")
       gameData[y][x] = PACMAN;
     }
   }
-
   function setupKeyboardControls() {
     document.addEventListener('keydown', function (e) {
       event.preventDefault();
@@ -270,159 +241,115 @@ $("")
   
       } else if (e.keyCode === 83){   // S key is 83 and 'down'
         movePacman('down');
-
       }
-
       // After every move, we erase the map and redraw it.
       eraseMap();
       drawMap();
-
       $(".pacman").append(backgroundDog);
     });
   }
-
-
-
-
-
-
-
-
-
-  //new code for timer  and stop/restart game
-
+  //new code for timer
   var timer;
+    
+    var game = {
   
-  var game = {
-
-  counter: 5,
-
-  start: function() {
-    timer = setInterval(game.countdown, 1000);
-
-    // $("#sub-wrapper").(game);
-
-    $("#timer").prepend("<h2>Time Remaining: <span id='counter-number'>5</span> s</h2>");
-
-    $("#start").remove();
-
-
-  },
-
-  countdown: function() {
-    $("#counter-number").html(game.counter);
-    if (game.counter === 0) {
-      clearInterval(timer);
-      $("#timer").html("Game Over!");
-
+    counter: 0,
+  
+    start: function() {
+      timer = setInterval(game.countdown, 1000);
+  
+      $("#timer").prepend("<h2>Time Remaining: <span id='counter-number'>0</span> s</h2>");
+  
+      $("#start").remove();
+    },
+    
+    countdown: function() {
+      $("#counter-number").html(game.counter);
+  
+      if (game.counter > 0) {
+        $("#highscore-div").html("Score: " + pacman.score);
+      }
+  
+      if (game.counter === 0) {
+        clearInterval(timer);
+        $("#timer").html("Game Over!");
+      }
+  
+      else (game.counter--)
+  
+    },
+  };
+  
+  
+  // function call for map and keyboard control functions
+    function main() {
+      drawMap();
+      setupKeyboardControls();
     }
-
-    else (game.counter--)
-
-  },
-
-
-
-  // done: function() {
-  //   $("#timer h2").remove();
-
-  // }
-
-
-};
-
-
-// function call for map and keyboard control functions
-
-  function main() {
-
-    drawMap();
-    setupKeyboardControls();
-  }
-
-  main();
-
-
-
-
-
-
-
-
-
-
-
-
+    main();
   
-// ON CLICK START EVENTS
-
-$(document).on("click", function() {
-
-  game.start();
-  $("#instructions").html();
-
-
   
-
-
-
-});
-
-  if (game.timer === 0) {
-
-  }
-
-  // YouTube 
-
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var playlistID = 'PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq';
-var queryURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq&autostart=1&controls=0&showinfo=0&fs=0&rel=0&key=AIzaSyDpHolcM49mui0yK8fCPRtjuCmazywXcJk"; 
-
-$.ajax({
-url:  queryURL,
-method: "GET"
-}).then(function(response) {
-      console.log(response);
- 
-
-   var player;
-   function onYouTubeIframeAPIReady() {
-       player = new YT.Player('ytplayer', {
-         width:'640',
-         height:'360',
-          videoId: 'J2DXnqPwGo',
-           playerVars: {
-             color: 'blue',
-               mute: 0,
-               autoplay: 1,
-               loop: 1,
-               controls: 0,
-               showinfo: 0,
-               autohide: 0,
-               enablejsapi: 1,
-               modestbranding: 0,
-               playlist: 'PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq',
-               vq: 'hd1080'
-           },
-           allowfullscreen: 0,
-           events: {
-          
-             'onStateChange': onPlayerStateChange
-           }
-       });
-   }
+  // CLICK START EVENTS
+  $("#start").on("click", function() {
+    game.counter = 30;
+    game.start();
+    $("#instructions").html();
+  
+  
   });
-   function onPlayerStateChange(el) {
-       if(el.data === 1) {
-           jQuery('#ytplayer').hide();
-       }
-   }
+    if (game.counter === 0) {
+      // document.getElementById("myDIV").removeEventListener("keydown", e);
+      $("#highscore-div").html("Score: " + pacman.score);
+    }
+    
   
-
-});
-
-
+  });
+  if (game.timer === 0) {
+  }
+//   // YouTube 
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var playlistID = 'PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq';
+// var queryURL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq&autostart=1&controls=0&showinfo=0&fs=0&rel=0&key=AIzaSyDpHolcM49mui0yK8fCPRtjuCmazywXcJk"; 
+// $.ajax({
+// url:  queryURL,
+// method: "GET"
+// }).then(function(response) {
+//       console.log(response);
+ 
+//    var player;
+//    function onYouTubeIframeAPIReady() {
+//        player = new YT.Player('ytplayer', {
+//          width:'640',
+//          height:'360',
+//           videoId: 'J2DXnqPwGo',
+//            playerVars: {
+//              color: 'blue',
+//                mute: 0,
+//                autoplay: 1,
+//                loop: 1,
+//                controls: 0,
+//                showinfo: 0,
+//                autohide: 0,
+//                enablejsapi: 1,
+//                modestbranding: 0,
+//                playlist: 'PLLmfuNiEhQopjCMvc_OyrIa1g9rkdXUKq',
+//                vq: 'hd1080'
+//            },
+//            allowfullscreen: 0,
+//            events: {
+          
+//              'onStateChange': onPlayerStateChange
+//            }
+//        });
+//    }
+//   });
+//    function onPlayerStateChange(el) {
+//        if(el.data === 1) {
+//            jQuery('#ytplayer').hide();
+//        }
+//    }
+  
+// });
